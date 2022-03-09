979f5a0cb6dfc0266bea001c6c86e8ca1c102f34
Pablo Rocha <pablorochatmm@gmail.com>
2022-03-08 21:05:27 -0600

    rows

--- Construccion de software/Desarrollo Web/1ra Clase/js/eventos_dom.js
+++ Construccion de software/Desarrollo Web/1ra Clase/js/eventos_dom.js
@@ -5,6 +5,17 @@ const main = () => {
 	};
 };
 
+let rowN = 3;
+const insertRow = () => {
+	let table = document.getElementById("sampleTable");
+	var row = table.insertRow(rowN - 1);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	cell1.innerHTML = `Row ${rowN} column 1`;
+	cell2.innerHTML = `Row ${rowN} column 1`;
+	rowN++;
+};
+
 onmousemove = function (e) {
 	let mouseText = document.getElementById("mousePosition");
 	mouseText.innerHTML = `Posición del mouse: X:${e.clientX} Y:${e.clientY}`;
