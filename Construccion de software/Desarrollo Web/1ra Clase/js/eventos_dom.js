cecad9ae224bb94b1755f251d7390957181ab65d
Pablo Rocha <pablorochatmm@gmail.com>
2022-03-08 21:59:27 -0600

    3

--- Construccion de software/Desarrollo Web/1ra Clase/js/eventos_dom.js
+++ Construccion de software/Desarrollo Web/1ra Clase/js/eventos_dom.js
@@ -5,18 +5,31 @@ const main = () => {
 	};
 };
 
-let rowN = 3;
-const insertRow = () => {
-	let table = document.getElementById("sampleTable");
-	var row = table.insertRow(rowN - 1);
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	cell1.innerHTML = `Row ${rowN} column 1`;
-	cell2.innerHTML = `Row ${rowN} column 1`;
-	rowN++;
-};
-
 onmousemove = function (e) {
 	let mouseText = document.getElementById("mousePosition");
 	mouseText.innerHTML = `Posición del mouse: X:${e.clientX} Y:${e.clientY}`;
+};
+
+let rowN = 2;
+let columnN = 2;
+const insertRow = () => {
+	let table = document.getElementById("sampleTable");
+	var row = table.insertRow(rowN);
+	for (let i = 0; i < columnN; i++) {
+		let cel = row.insertCell(i);
+		cel.innerHTML = `Row ${rowN + 1} column ${i + 1}`;
+	}
+	rowN++;
+};
+
+const insertColumn = () => {
+	let table = document.getElementById("sampleTable");
+
+	for (let i = 0; i < rowN; i++) {
+		const row = table.rows[i];
+		let cel = row.insertCell(columnN);
+		cel.innerHTML = `Row ${i + 1} column ${columnN + 1}`;
+	}
+
+	columnN++;
 };
